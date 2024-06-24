6ed42f8145ed32aed4b8dd256544bc0b3cae14ee
Omid Zaheri <omid.zaheri380@gmail.com>
2024-06-24 15:46:22 +0330

    Initial Commit

---
+++ Apple-Framework/Apple_FrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworkApp.swift
+//  Apple-Framework
+//
+//  Created by Omid Zaheri on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
